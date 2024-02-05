ca8be0cddf631fab0550b5e262ce0afe823907ec
Eric Bower <me@erock.io>
2024-02-05 14:24:03 -0500

    chore: github action

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ set -o errexit
 set -x
 
 PORT="${PORT:-5000}"
+SERVICE="${SERVICE:-imgs.sh}"
 
 echo -e "$PRIVATE_KEY" > ./key
 chmod 600 ./key
-exec autossh -M 0 -N -L 0.0.0.0:"$PORT":localhost:"$PORT" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i ./key "$USERNAME"@imgs.sh
+exec autossh -M 0 -N -L 0.0.0.0:"$PORT":localhost:"$PORT" -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i ./key "$USERNAME"@"$SERVICE"
