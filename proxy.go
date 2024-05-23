8c13ec56b247d10de61e30b62c04b7dff2db20d1
Eric Bower <me@erock.io>
2024-05-23 14:55:37 -0400

    changes

---
+++ proxy.go
@@ -0,0 +1,46 @@
+package ptun
+
+import (
+	"log/slog"
+	"net"
+
+	"github.com/charmbracelet/ssh"
+	gossh "golang.org/x/crypto/ssh"
+)
+
+type ProxyTunnelHandler struct {
+	Logger *slog.Logger
+}
+
+func (tun *ProxyTunnelHandler) GetLogger() *slog.Logger {
+	return tun.Logger
+}
+
+func (tun *ProxyTunnelHandler) CreateListener(ctx ssh.Context) (net.Listener, error) {
+	return nil, nil
+}
+
+func (tun *ProxyTunnelHandler) CreateConn(ctx ssh.Context) (net.Conn, error) {
+	rawConn, err := net.Dial("tcp", "pico.sh:22")
+	if err != nil {
+		return nil, err
+	}
+
+	sshConn, chans, reqs, err := gossh.NewClientConn(rawConn, "pico.sh", &gossh.ClientConfig{})
+	if err != nil {
+		return nil, err
+	}
+
+	sshClient := gossh.NewClient(sshConn, chans, reqs)
+
+	dbConn, err := sshClient.Dial("unix", "/var/run/port-forward/2c537858")
+	if err != nil {
+		return nil, err
+	}
+
+	return dbConn, nil
+}
+
+func (tun *ProxyTunnelHandler) Serve(listener net.Listener, ctx ssh.Context) error {
+	return nil
+}
